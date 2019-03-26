6af0acbd18fa6f66e76bc378b91c24cb566d93af
AdrianGerber <43301733+AdrianGerber@users.noreply.github.com>
2019-03-26 21:07:07 +0100

    Added color class
    Added a class to operate with colors and inserted the corresponding test methods

---
+++ TheRayTracerChallenge/Color.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "Constants.h"
+
+class Color
+{
+public:
+    float r, g, b;
+
+    Color();
+    Color(float r, float g, float b);
+    ~Color() = default;
+    
+    //Operator overloads
+    bool operator== (Color c);
+    bool operator!= (Color c);
+    Color operator+( Color c);
+    Color operator- (Color c);
+    Color operator* (float scalar);
+    Color operator* (Color c);
+};
+
+Color::Color()
+{
+    r = g = b = 0.0f;
+}
+
+inline Color::Color(float r, float g, float b)
+{
+    this->r = r;
+    this->g = g;
+    this->b = b;
+}
+
+inline bool Color::operator==(Color c)
+{
+    return
+        Constants::FloatEqual(r, c.r)
+        && Constants::FloatEqual(g, c.g)
+        && Constants::FloatEqual(b, c.b);
+}
+
+inline bool Color::operator!=(Color c)
+{
+    return !(*this == c);
+}
+
+inline Color Color::operator+(Color c)
+{
+    return Color(r + c.r, g + c.g, b + c.b);
+}
+
+inline Color Color::operator-(Color c)
+{
+    return Color(r - c.r, g - c.g, b - c.b);
+}
+
+inline Color Color::operator*(float scalar)
+{
+    return Color(
+        r * scalar,
+        g * scalar,
+        b * scalar
+    );
+}
+
+//Calculate the hadamard product
+inline Color Color::operator*(Color c)
+{
+    return Color(
+        r * c.r,
+        g * c.g,
+        b * c.b
+    );
+}
---
+++ TheRayTracesChallengeTests/ColorTests.cpp
@@ -0,0 +1,63 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../TheRayTracerChallenge/Color.h"
+#include "../TheRayTracerChallenge/Constants.h"
+#include <cmath>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TheRayTracesChallengeTests
+{
+    TEST_CLASS(ColorTests)
+    {
+    public:
+
+        TEST_METHOD(ColorCreation)
+        {
+            Color c(-0.5f, 0.4f, 1.7f);
+
+            Assert::IsTrue(Constants::FloatEqual(c.r, -0.5));
+            Assert::IsTrue(Constants::FloatEqual(c.g, 0.4f));
+            Assert::IsTrue(Constants::FloatEqual(c.b, 1.7f));
+        }
+
+        TEST_METHOD(ColorComparison) {
+            Color c1(0.1f, 0.2f, 0.3f);
+            Color c2(0.2f, 0.5f, -12.0f);
+            Color c3(0.1f, 0.2f, 0.3f);
+
+            Assert::IsTrue(c1 == c3);
+            Assert::IsFalse(c1 != c3);
+            Assert::IsFalse(c1 == c2);
+            Assert::IsTrue(c1 != c2);
+
+
+        }
+
+        TEST_METHOD(ColorAddition) {
+            Color c1(0.9f, 0.6f, 0.75f);
+            Color c2(0.7f, 0.1f, 0.25f);
+
+            Assert::IsTrue(c1 + c2 == Color(1.6f, 0.7f, 1.0f));
+        }
+        TEST_METHOD(ColorSubtraction) {
+            Color c1(0.9f, 0.6f, 0.75f);
+            Color c2(0.7f, 0.1f, 0.25f);
+
+            Assert::IsTrue(c1 - c2 == Color(0.2f, 0.5f, 0.5f));
+        }
+
+        TEST_METHOD(ColorScalarMultiplication) {
+            Color c(0.2f, 0.3f, 0.4f);
+
+            Assert::IsTrue(c * 2.0f == Color(0.4f, 0.6f, 0.8f));
+        }
+
+        TEST_METHOD(ColorMultiplication) {
+            Color c1(1.0f, 0.2f, 0.4f);
+            Color c2(0.9f, 1.0f, 0.1f);
+
+            Assert::IsTrue(c1 * c2 == Color(0.9f, 0.2f, 0.04f));
+        }
+    };
+}
