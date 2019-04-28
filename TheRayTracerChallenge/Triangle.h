5114189a81c9a85ab52c5ee999a7de1a48a1976c
AdrianGerber <43301733+AdrianGerber@users.noreply.github.com>
2019-04-28 21:05:19 +0200

    Added Triangles

--- TheRayTracerChallenge/Triangle.h
+++ TheRayTracerChallenge/Triangle.h
@@ -5,32 +5,93 @@
 class Triangle : public Shape {
 public:
 
-	void FindObjectSpaceIntersections(Ray ray, IntersectionBuffer& buffer) override { return; }
+	Triangle() = default;
+	Triangle(Point point0, Point point1, Point point2);
+	~Triangle() = default;
 
-	Vector FindObjectSpaceNormal(Point p) override { return Vector();}
+	void FindObjectSpaceIntersections(Ray ray, IntersectionBuffer& buffer) override;
+
+	Vector FindObjectSpaceNormal(Point p) override { return normal; }
 
 	void PartitionChildren(size_t maximumShapeCount) override {}
 
-	BoundingBox GetObjectSpaceBounds() override {
-		/*
-		function bounds_of(triangle)
-  let box ? bounding_box(empty)
+	BoundingBox GetObjectSpaceBounds() override;
 
-  add triangle.p1 to box
-  add triangle.p2 to box
-  add triangle.p3 to box
-
-  return box
-end function
-*/
-
-
-
-		return BoundingBox();
-	}
+	Point GetPoint(size_t nr);
+	Vector GetEdge(size_t nr);
 
 private:
+	Point p0, p1, p2;
+	Vector e0, e1, normal;
+
+	void CalculateAttributes();
+
 	std::shared_ptr<Shape> ShapeSpecificCopy() override {
 		return Shape::MakeShared<Triangle>(*this);
 	}
 };
+
+inline Triangle::Triangle(Point point0, Point point1, Point point2) {
+	p0 = point0;
+	p1 = point1;
+	p2 = point2;
+
+	CalculateAttributes();
+}
+
+inline Point Triangle::GetPoint(size_t nr) {
+	switch (nr) {
+	case 0: return p0;
+	case 1: return p1;
+	case 2: return p2;
+	default: return p0;
+	}
+}
+
+inline Vector Triangle::GetEdge(size_t nr) {
+	switch (nr) {
+	case 0: return e0;
+	case 1: return e1;
+	default: return e0;
+	}
+}
+
+inline void Triangle::CalculateAttributes() {
+	e0 = p1 - p0;
+	e1 = p2 - p0;
+
+	normal = Vector::CrossProduct(e1, e0).Normalize();
+}
+
+inline BoundingBox Triangle::GetObjectSpaceBounds() {
+	BoundingBox bounds;
+
+	bounds.Add(p0);
+	bounds.Add(p1);
+	bounds.Add(p2);
+
+	return bounds;
+}
+
+inline void Triangle::FindObjectSpaceIntersections(Ray ray, IntersectionBuffer& buffer) {
+	auto dirCrossE1 = Vector::CrossProduct(ray.direction, e1);
+	double det = Vector::DotProduct(e0, dirCrossE1);
+	//No intersection
+	if (fabs(det) < Constants::EPSILON) return;
+
+	double f = 1.0 / det;
+	Vector p0ToOrigin = ray.origin - p0;
+	double u = f * Vector::DotProduct(p0ToOrigin, dirCrossE1);
+	//No intersection
+	if (u < 0.0 || u > 1.0) return;
+
+	auto originCrossE0 = Vector::CrossProduct(p0ToOrigin, e0);
+	double v = f * Vector::DotProduct(ray.direction, originCrossE0);
+	//No intersection
+	if (v < 0.0 || u + v > 1.0) return;
+
+	double t = f * Vector::DotProduct(e1, originCrossE0);
+
+	//Ray intersects triangle
+	buffer.Add(Intersection(t, GetPointer()));
+}
--- TheRayTracerChallengeTests/TriangleTests.cpp
+++ TheRayTracerChallengeTests/TriangleTests.cpp
@@ -10,6 +10,7 @@
 #include <string>
 #include <Shape.h>
 #include <Triangle.h>
+#include <BoundingBox.h>
 #include <type_traits>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -26,20 +27,68 @@ namespace TheRayTracesChallengeTests
 			Assert::IsTrue(std::is_base_of<Shape, Triangle>());
 		}
 
-		/*Bounding Box
-		
-		Scenario: A triangle has a bounding box
-		Given p1 ← point(-3, 7, 2)
-		And p2 ← point(6, 2, -4)
-		And p3 ← point(2, -1, -1)
-		And shape ← triangle(p1, p2, p3)
-		When box ← bounds_of(shape)
-		Then box.min = point(-3, -1, -4)
-		And box.max = point(6, 7, 2)
-		
-		
-		
-		*/
+		TEST_METHOD(Creation) {
+			auto p0 = Point::CreatePoint(0.0, 1.0, 0.0);
+			auto p1 = Point::CreatePoint(-1.0, 0.0, 0.0);
+			auto p2 = Point::CreatePoint(1.0, 0.0, 0.0);
 
+			Triangle triangle(p0, p1, p2);
+
+			Assert::IsTrue(triangle.GetPoint(0) == p0);
+			Assert::IsTrue(triangle.GetPoint(1) == p1);
+			Assert::IsTrue(triangle.GetPoint(2) == p2);
+
+			Assert::IsTrue(triangle.GetEdge(0) == Vector::CreateVector(-1.0, -1.0, 0.0));
+			Assert::IsTrue(triangle.GetEdge(1) == Vector::CreateVector(1.0, -1.0, 0.0));
+			Assert::IsTrue(triangle.FindObjectSpaceNormal(p0) == Vector::CreateVector(0.0, 0.0, -1.0));
+		}
+		TEST_METHOD(BoundingBox) {
+			Triangle triangle(
+				Point::CreatePoint(-3.0, 7.0, 2.0),
+				Point::CreatePoint(6.0, 2.0, -4.0),
+				Point::CreatePoint(2.0, -1.0, -1.0)
+			);
+
+			auto bounds = triangle.GetObjectSpaceBounds();
+			Assert::IsTrue(bounds.GetMin() == Point::CreatePoint(-3.0, -1.0, -4.0));
+			Assert::IsTrue(bounds.GetMax() == Point::CreatePoint(6.0, 7.0, 2.0));
+		}
+
+		TEST_METHOD(RayIntersections) {
+			auto triangle = Shape::MakeShared<Triangle>(
+				Point::CreatePoint(0.0, 1.0, 0.0),
+				Point::CreatePoint(-1.0, 0.0, 0.0),
+				Point::CreatePoint(1.0, 0.0, 0.0)
+			);
+
+			std::vector <std::pair<Ray, IntersectionBuffer>> tests = {
+				//Ray parallel to triangle
+				{Ray(Point::CreatePoint(0.0, 1.0, -2.0), Vector::CreateVector(0.0, 1.0, 0.0)), IntersectionBuffer()},
+				//Ray misses p0-p2 edge
+				{Ray(Point::CreatePoint(1.0, 1.0, -2.0), Vector::CreateVector(0.0, 0.0, 1.0)), IntersectionBuffer()},
+				//Ray misses p0-p1 edge
+				{Ray(Point::CreatePoint(-1.0, 1.0, -2.0), Vector::CreateVector(0.0, 0.0, 1.0)), IntersectionBuffer()},
+				//Ray misses p1-p2 edge
+				{Ray(Point::CreatePoint(0.0, -1.0, -2.0), Vector::CreateVector(0.0, 0.0, 1.0)), IntersectionBuffer()},
+				//Ray hits the triangle
+				{Ray(Point::CreatePoint(0.0, 0.5, -2.0), Vector::CreateVector(0.0, 0.0, 1.0)), IntersectionBuffer(Intersection(2.0, triangle))}
+			};
+
+			for (auto& test : tests) {
+				auto ray = test.first;
+				auto expected = test.second;
+				IntersectionBuffer result;
+
+				triangle->FindObjectSpaceIntersections(ray, result);
+
+				Assert::IsTrue(expected.GetCount() == result.GetCount());
+
+				if (expected.GetCount() > 0) {
+					for (size_t i = 0; i < expected.GetCount(); i++) {
+						Assert::IsTrue(expected[i] == result[i]);
+					}
+				}
+			}
+		}
 	};
 }
